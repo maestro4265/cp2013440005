b6747b800ef7244061f7131173fd6f8c13b5c7e4
2013440005 <maestro4265@gmail.com>
2018-10-31 15:10:31 +0900

    HW3

--- ex3-10/ex3-10.cpp
+++ ex3-10/ex3-10.cpp
@@ -3,18 +3,17 @@
 void changeindex_array(int arr[], int len)
 {
 	int i;
-	int index;
 	int num1=arr[0];
-	int num2=arr[len-1];
+	int num2=arr[len-3];
 	
+	arr[0]=num2;
+	arr[len-3]=num1;
+
 	for(i=0;i<len;i++)
 	{
-		if(i==len-1)
-			arr[0]=num2;
-			arr[len-1]=num1;
+	printf("%d ", arr[i]);
 	}
 	
-	printf("%d", arr);
 	return ;
 }
 
@@ -22,11 +21,15 @@ int main()
 {
 	int len=5;
 	int arr[]={20, 100, 15000, 15, 20};
-	int num;
+	int i;
 
-	num=changeindex_array(arr, len);
+	for(i=0;i<len;i++)
+	{
+	printf("%d ", arr[i]);
+	}
+	printf("\n");
 
-	printf("가장 큰 정수의 index = %d \n", num);
-
+	changeindex_array(arr, len);	
+	
 	return 0;
 }
